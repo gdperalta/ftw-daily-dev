86a8bb14c87fdf807da98026c942d6a8d5b8b847
gdperalta <godfreydjperalta@gmail.com>
2022-07-01 16:10:58 +0800

    Add cleaning fee constant and pass as prop to BookingDatesForm

--- src/components/BookingPanel/BookingPanel.js
+++ src/components/BookingPanel/BookingPanel.js
@@ -78,6 +78,9 @@ const BookingPanel = props => {
   const showClosedListingHelpText = listing.id && isClosed;
   const { formattedPrice, priceTitle } = priceData(price, intl);
   const isBook = !!parse(location.search).book;
+  const cleaningFee = listing.attributes.publicData && listing.attributes.publicData.cleaningFee
+                      ? listing.attributes.publicData.cleaningFee
+                      : null;
 
   const subTitleText = !!subTitle
     ? subTitle
@@ -134,6 +137,7 @@ const BookingPanel = props => {
             lineItems={lineItems}
             fetchLineItemsInProgress={fetchLineItemsInProgress}
             fetchLineItemsError={fetchLineItemsError}
+            cleaningFee={cleaningFee}
           />
         ) : null}
       </ModalInMobile>
